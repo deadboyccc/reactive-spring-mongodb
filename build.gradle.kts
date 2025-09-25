c29bb8574b34c1d334257d3542f152e337847589
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-25 07:04:58 +0300

    coroutine aware router + crud

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-//    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
