fc85944fd6002f9fb33006e49a2a5abe5f2b2a2b
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-25 05:47:39 +0300

    coroutine aware router

--- src/main/kotlin/dev/dead/reactivespringmongodb/ReactiveSpringMongodbApplication.kt
+++ src/main/kotlin/dev/dead/reactivespringmongodb/ReactiveSpringMongodbApplication.kt
@@ -1,13 +1,17 @@
 package dev.dead.reactivespringmongodb
 
+import kotlinx.coroutines.reactive.asFlow
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.coRouter
 import reactor.core.publisher.Flux
 
 @Document
@@ -23,7 +27,28 @@ class CustomerRestController(val customerRepository: CustomerRepository) {
 }
 
 @SpringBootApplication
-class ReactiveSpringMongodbApplication
+class ReactiveSpringMongodbApplication {
+    @Bean
+    fun routes(customerRepository: CustomerRepository) = coRouter {
+        GET("/customers") {
+            ServerResponse.ok()
+                .bodyAndAwait(
+                    customerRepository
+                        .findAll().asFlow()
+                )
+        }
+    }
+    //java way
+//    @Bean
+//    fun routes(customerRepository: CustomerRepository): RouterFunction<ServerResponse?> {
+//
+//            return route()
+//                .GET("/customers", HandlerFunction<ServerResponse> {
+//                    ServerResponse.ok().body(customerRepository.findAll())
+//                })
+//                .build();
+//        }
+}
 
 fun main(args: Array<String>) {
     runApplication<ReactiveSpringMongodbApplication>(*args)
